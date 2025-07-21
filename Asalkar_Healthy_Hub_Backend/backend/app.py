e6f34a33959bb692e727cfff8224e95b2aa70be0
Sumit_7 <asalkarsumit@gmail.com>
2025-07-21 22:08:33 +0530

    Update app.py

--- Asalkar_Healthy_Hub_Backend/backend/app.py
+++ Asalkar_Healthy_Hub_Backend/backend/app.py
@@ -11,7 +11,7 @@ from urllib.parse import quote  # ✅ ADD THIS NEW IMPORT
 app = Flask(__name__, static_folder="../static")
 
 # ✅ Apply CORS to the entire app
-CORS(app, origins=["https://ecommerce-brown-xi.vercel.app"])
+CORS(app, origins=["*"]) 
 
 # ✅ Set upload folder path
 app.config['UPLOAD_FOLDER'] = 'static/images/products'
